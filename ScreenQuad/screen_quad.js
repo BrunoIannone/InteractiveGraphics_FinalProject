b013e7f5ea20ceb3db86d7cb19e1c5a2563e7a8c
Bruno_Iannone <iannone.1883472@studenti.uniroma1.it>
2024-06-30 17:44:49 +0200

    Sphere + background working

---
+++ ScreenQuad/screen_quad.js
@@ -0,0 +1,139 @@
+
+function InitShaderProgramFromScripts( vs, fs )
+{
+	return InitShaderProgram( document.getElementById(vs).text, document.getElementById(fs).text );	
+}
+
+// This is a helper function for compiling the given vertex and fragment shader source code into a program.
+function InitShaderProgram( vsSource, fsSource )
+{
+	const vs = CompileShader( gl.VERTEX_SHADER,   vsSource );
+	const fs = CompileShader( gl.FRAGMENT_SHADER, fsSource );
+
+	if ( ! vs || ! fs ) return null;
+	
+	const prog = gl.createProgram();
+	gl.attachShader(prog, vs);
+	gl.attachShader(prog, fs);
+	gl.linkProgram(prog);
+
+	if (!gl.getProgramParameter(prog, gl.LINK_STATUS)) {
+		alert('Unable to initialize the shader program: ' + gl.getProgramInfoLog(prog));
+		return null;
+	}
+	return prog;
+}
+
+// This is a helper function for compiling a shader, called by InitShaderProgram().
+function CompileShader( type, source )
+{
+	const shader = gl.createShader(type);
+	gl.shaderSource(shader, source);
+	gl.compileShader(shader);
+	if (!gl.getShaderParameter( shader, gl.COMPILE_STATUS) ) {
+		alert('An error occurred compiling shader:\n' + gl.getShaderInfoLog(shader));
+		gl.deleteShader(shader);
+		return null;
+	}
+	return shader;
+}
+class ScreenQuad{
+
+	constructor(gl)
+	{
+		this.gl = gl;
+		if ( ! this.vbuf ) this.vbuf = this.gl.createBuffer();
+	}
+	updateProj(fov,z){
+		const r = canvas.width / canvas.height;
+		const ff = Math.PI * fov / 180;
+		const tant_2 = Math.tan( ff/2 );
+		const y = z * tant_2;
+		const x = y * r;
+		const rtp = [
+			-x, -y, -z,
+			 x, -y, -z,
+			 x,  y, -z,
+			-x, -y, -z,
+			 x,  y, -z,
+			-x,  y, -z,
+		];
+		this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.vbuf);
+		this.gl.bufferData(this.gl.ARRAY_BUFFER, new Float32Array(rtp), this.gl.STATIC_DRAW);
+	}
+	draw( prog, trans )
+	{
+
+		this.gl.useProgram( prog );
+		this.gl.uniformMatrix4fv( this.gl.getUniformLocation( prog, 'c2w' ), false, trans.camToWorld );
+		this.gl.bindBuffer( this.gl.ARRAY_BUFFER, this.vbuf );
+		var p = this.gl.getAttribLocation ( prog, 'p' );
+		this.gl.vertexAttribPointer( p, 3, this.gl.FLOAT, false, 0, 0 );
+		this.gl.enableVertexAttribArray( p );
+
+		this.gl.drawArrays( this.gl.TRIANGLES, 0, 6 );
+	}
+};
+class BackGround
+{
+	constructor(gl)
+	{
+		this.gl = gl
+		this.prog = InitShaderProgramFromScripts( 'raytraceVS', 'envFS' );
+		this.screenQuad = new ScreenQuad(this.gl)
+	}
+	updateProj(fov,z)
+	{
+		gl.useProgram( this.prog );
+		gl.uniformMatrix4fv( gl.getUniformLocation( this.prog, 'proj' ), false, perspectiveMatrix );
+		this.screenQuad.updateProj(fov,z)
+	}
+	draw( trans )
+	{
+		gl.depthMask( false );
+		this.screenQuad.draw( this.prog, trans );
+		gl.depthMask( true );
+	}
+};
+function InitEnvironmentMap()
+{
+	environmentTexture = gl.createTexture();
+	gl.bindTexture( gl.TEXTURE_CUBE_MAP, environmentTexture );
+	
+	const url = 'http://localhost:3000/';
+	const files = [
+	  'px.png',
+	  'nx.png',
+	  'py.png',
+	  'ny.png',
+	  'pz.png',
+	  'nz.png',
+	];
+	const faces = [
+		gl.TEXTURE_CUBE_MAP_POSITIVE_X,
+		gl.TEXTURE_CUBE_MAP_NEGATIVE_X,
+		gl.TEXTURE_CUBE_MAP_POSITIVE_Y,
+		gl.TEXTURE_CUBE_MAP_NEGATIVE_Y,
+		gl.TEXTURE_CUBE_MAP_POSITIVE_Z,
+		gl.TEXTURE_CUBE_MAP_NEGATIVE_Z,
+	];
+
+	var loaded = 0;
+	for ( var i=0; i<6; ++i ) {
+		gl.texImage2D( faces[i], 0, gl.RGBA, 128, 128, 0, gl.RGBA, gl.UNSIGNED_BYTE, null );
+		const img = new Image();
+		img.crossOrigin = "anonymous";
+		img.face = faces[i];
+		img.onload = function() {
+			gl.bindTexture( gl.TEXTURE_CUBE_MAP, environmentTexture );
+			gl.texImage2D( this.face, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, this );
+			loaded++;
+			if ( loaded == 6 ) {
+				gl.generateMipmap(gl.TEXTURE_CUBE_MAP);
+				DrawScene();
+			}
+		};
+		img.src = url + files[i];
+	}
+	gl.texParameteri( gl.TEXTURE_CUBE_MAP, gl.TEXTURE_MIN_FILTER, gl.LINEAR_MIPMAP_LINEAR );
+}
---
+++ Spheres/sphere.js
@@ -0,0 +1,172 @@
+class TriSphereClass{
+    constructor(subdiv,gl){
+        this.gl = gl
+        var b = this.TriSphere(subdiv);
+		this.pbuf = this.gl.createBuffer();
+		this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.pbuf);
+		this.gl.bufferData(this.gl.ARRAY_BUFFER, new Float32Array(b.pos), this.gl.STATIC_DRAW);
+		this.ebuf = this.gl.createBuffer();
+		this.gl.bindBuffer(this.gl.ELEMENT_ARRAY_BUFFER, this.ebuf);
+		this.gl.bufferData(this.gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(b.elems), gl.STATIC_DRAW);
+		this.count = b.elems.length;
+    }
+    TriSphere(subdiv){
+    
+	var faces = [];
+	var verts = [];
+	verts.push(0,0, 1);
+	verts.push(0,0,-1);
+	var vpt = 0;
+	var vpb = 1;
+	var vi = 2;
+	for ( var i=1; i<subdiv; ++i ) {
+		var a = Math.PI * i / (2*subdiv);
+		var z = Math.cos(a);
+		var r = Math.sin(a);
+		a = 0;
+		var da = Math.PI / (2*i);
+		var v0t = vpt;
+		var v0b = vpb;
+		var v1t = vi++;
+		var v1b = vi++;
+		verts.push(r,0, z);
+		verts.push(r,0,-z);
+		for ( var s=0; s<4; ++s ) {
+			for ( var j=1; j<i; ++j ) {
+				a += da;
+				var x = Math.cos(a)*r;
+				var y = Math.sin(a)*r;
+				verts.push( x, y,  z );
+				verts.push( x, y, -z );
+				faces.push( v0t, vi-2, vi );
+				faces.push( v0t, vi, v0t+2 );
+				faces.push( v0b, vi-1, vi+1 );
+				faces.push( v0b, vi+1, v0b+2 );
+				v0t+=2;
+				v0b+=2;
+				vi+=2;
+			}
+			if ( s < 3 ) {
+				a += da;
+				var x = Math.cos(a)*r;
+				var y = Math.sin(a)*r;
+				verts.push( x, y,  z );
+				verts.push( x, y, -z );
+				faces.push( v0t, vi-2, vi );
+				faces.push( v0b, vi-1, vi+1 );
+				vi+=2;
+			}
+		}
+		if ( i > 1 ) {
+			faces[ faces.length-7 ] = vpt;
+			faces[ faces.length-1 ] = vpb;
+		}
+		faces.push( vpt, vi-2, v1t );
+		faces.push( vpb, vi-1, v1b );
+		vpt = v1t;
+		vpb = v1b;
+	}
+	var a = 0;
+	var da = Math.PI / (2*subdiv);
+	verts.push(1,0,0);
+	var v0t = vpt;
+	var v0b = vpb;
+	var v1 = vi++;
+	for ( var s=0; s<4; ++s ) {
+		for ( var j=1; j<subdiv; ++j ) {
+			a += da;
+			var x = Math.cos(a);
+			var y = Math.sin(a);
+			verts.push( x, y, 0 );
+			faces.push( v0t, vi-1, vi );
+			faces.push( v0t, vi, v0t+2 );
+			faces.push( v0b, vi-1, vi );
+			faces.push( v0b, vi, v0b+2 );
+			v0t+=2;
+			v0b+=2;
+			vi++;
+		}
+		if ( s < 3 ) {
+			a += da;
+			var x = Math.cos(a);
+			var y = Math.sin(a);
+			verts.push( x, y, 0 );
+			faces.push( v0t, vi-1, vi );
+			faces.push( v0b, vi-1, vi );
+			vi++;
+		}
+	}
+	if ( subdiv > 1 ) {
+		faces[ faces.length-7 ] = vpt;
+		faces[ faces.length-1 ] = vpb;
+	}
+	faces.push( vpt, vi-1, v1 );
+	faces.push( vpb, vi-1, v1 );
+	return { pos:verts, elems:faces };
+}
+draw( vp )
+	{
+		gl.bindBuffer( gl.ARRAY_BUFFER, this.pbuf );
+		gl.vertexAttribPointer( vp, 3, gl.FLOAT, false, 0, 0 );
+		gl.enableVertexAttribArray( vp );
+		gl.bindBuffer( gl.ELEMENT_ARRAY_BUFFER, this.ebuf );
+		gl.drawElements( gl.TRIANGLES, this.count, gl.UNSIGNED_SHORT, 0 );
+	}
+}
+
+class SphereProg
+{
+	constructor(subdiv,gl)
+	{
+        this.gl = gl;
+        this.subdiv = subdiv
+        
+        console.log(this.gl);
+		
+    }
+    init(){
+        this.mvp     = this.gl.getUniformLocation( this.prog, 'mvp' );
+		this.campos  = this.gl.getUniformLocation( this.prog, 'campos' );
+		this.center  = this.gl.getUniformLocation( this.prog, 'center' );
+		this.radius  = this.gl.getUniformLocation( this.prog, 'radius' );
+		this.mtl_k_d = this.gl.getUniformLocation( this.prog, 'mtl.k_d' );
+		this.mtl_k_s = this.gl.getUniformLocation( this.prog, 'mtl.k_s' );
+		this.mtl_n   = this.gl.getUniformLocation( this.prog, 'mtl.n' );
+		this.vp      = this.gl.getAttribLocation ( this.prog, 'p' );
+        this.triSphere = new TriSphereClass(this.subdiv,this.gl);
+	}
+	setTrans( mvp, campos )
+	{
+		this.gl.useProgram( this.prog );
+		this.gl.uniformMatrix4fv( this.mvp, false, mvp );
+		this.gl.uniform3fv( this.campos, campos );
+	}
+	setLight( pos, intens )
+	{
+		this.gl.useProgram( this.prog );
+		this.gl.uniform3fv( this.gl.getUniformLocation( this.prog, 'light.position'  ), pos    );
+		this.gl.uniform3fv( this.gl.getUniformLocation( this.prog, 'light.intensity' ), intens );
+	}
+	draw( sphere )
+	{
+		this.gl.useProgram( this.prog );
+		this.gl.uniform3fv( this.center,  sphere.center  );
+		this.gl.uniform1f ( this.radius,  sphere.radius  );
+		this.gl.uniform3fv( this.mtl_k_d, sphere.mtl.k_d );
+		this.gl.uniform3fv( this.mtl_k_s, sphere.mtl.k_s );
+		this.gl.uniform1f ( this.mtl_n,   sphere.mtl.n   );
+		this.triSphere.draw( this.vp );
+	}
+};
+
+class SphereDrawer extends SphereProg
+{
+	constructor(subdiv, gl)
+	{
+        super(subdiv, gl);
+
+        this.prog = InitShaderProgramFromScripts( 'sphereVS', 'sphereFS' );
+        
+		this.init();
+	}
+};
