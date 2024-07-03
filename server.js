c840daa2c6751fd1849126528f73f291828f95ae
Bruno_Iannone <iannone.1883472@studenti.uniroma1.it>
2024-07-03 17:42:41 +0200

    Photots

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+
+// Middleware per consentire le richieste CORS da tutte le origini durante lo sviluppo
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*'); // Consente le richieste da tutte le origini
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  next();
+});
+
+app.use(express.static('/home/bruno/Scaricati'));
+
+app.listen(3000, () => {
+  console.log('Server avviato su http://localhost:3000/');
+});
