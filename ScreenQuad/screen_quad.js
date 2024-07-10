dfd39c7e4167c252473397af71007f8902e0c0f7
Bruno_Iannone <iannone.1883472@studenti.uniroma1.it>
2024-07-10 12:54:56 +0200

    Improving code and folder

--- ScreenQuad/screen_quad.js
+++ ScreenQuad/screen_quad.js
@@ -1,42 +1,4 @@
 
-/*function InitShaderProgramFromScripts( vs, fs )
-{
-	return InitShaderProgram( document.getElementById(vs).text, document.getElementById(fs).text );	
-}
-
-// This is a helper function for compiling the given vertex and fragment shader source code into a program.
-function InitShaderProgram( vsSource, fsSource )
-{
-	const vs = CompileShader( gl.VERTEX_SHADER,   vsSource );
-	const fs = CompileShader( gl.FRAGMENT_SHADER, fsSource );
-
-	if ( ! vs || ! fs ) return null;
-	
-	const prog = gl.createProgram();
-	gl.attachShader(prog, vs);
-	gl.attachShader(prog, fs);
-	gl.linkProgram(prog);
-
-	if (!gl.getProgramParameter(prog, gl.LINK_STATUS)) {
-		alert('Unable to initialize the shader program: ' + gl.getProgramInfoLog(prog));
-		return null;
-	}
-	return prog;
-}
-
-// This is a helper function for compiling a shader, called by InitShaderProgram().
-function CompileShader( type, source )
-{
-	const shader = gl.createShader(type);
-	gl.shaderSource(shader, source);
-	gl.compileShader(shader);
-	if (!gl.getShaderParameter( shader, gl.COMPILE_STATUS) ) {
-		alert('An error occurred compiling shader:\n' + gl.getShaderInfoLog(shader));
-		gl.deleteShader(shader);
-		return null;
-	}
-	return shader;
-}*/
 class ScreenQuad{
 
 	constructor(gl)
@@ -78,8 +40,6 @@ class BackGround
 	constructor(gl)
 	{
 		this.gl = gl
-		//this.InitEnvironmentMap()
-
 		this.prog = InitShaderProgram( BackVS, BackFS );
 		this.screenQuad = new ScreenQuad(this.gl)
 	}
@@ -146,7 +106,6 @@ class BackGround
 		}
 };
 
-
 BackVS = 
 
 		`
@@ -164,7 +123,6 @@ BackVS =
 			vec4 rd = c2w * vec4(p,1);
 			ray_dir = rd.xyz - ray_pos;
 		}`;
-
 
 BackFS = 
 `
